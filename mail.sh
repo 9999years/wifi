dbb73fef3212337768686bd4a8f3f34511ff8c9a
Rebecca Turner <637275@gmail.com>
2018-09-13 20:51:31 -0400

    cron bullshit

--- mail.sh
+++ mail.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# sdoigfjas;ldgkja;lsdgja;  fuck cron
+PATH="/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 EMAIL_TO="Rebecca Turner <637275@gmail.com>"
 IP="$(./get_ip.sh)"
 if [[ ! -z "$(echo "$IP" | diff .ip -)" ]]
