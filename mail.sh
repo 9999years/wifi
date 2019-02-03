a11514f0087d35c55ac49b9de3481545c4c26a2f
Rebecca Turner <637275@gmail.com>
2019-02-03 17:55:09 -0500

    kinda verbose option, better storage of ip in /run/user...

--- mail.sh
+++ mail.sh
@@ -1,12 +1,30 @@
 #! /bin/bash
 
+if [[ "$1" == "-v" ]]
+then
+	# verbose
+	VERBOSE=1
+fi
+
 # sdoigfjas;ldgkja;lsdgja;  fuck cron
 PATH="/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 EMAIL_TO="Rebecca Turner <637275@gmail.com>"
 IP="$(./get_ip.sh)"
-if [[ ! -z "$(echo "$IP" | diff .ip -)" ]]
+IP_FILE="/run/user/$(id -u)/ip.txt"
+
+if [[ ! -e "$IP_FILE" ]]
+then
+	touch "$IP_FILE"
+fi
+
+if [[ ! -z "$(echo "$IP" | diff "$IP_FILE" -)" ]]
 then
 	# new ip
-	echo "$IP" > .ip
-	./info.sh | sendmail -v "$EMAIL_TO" > /dev/null
+	echo "$IP" > "$IP_FILE"
+	if [[ ! -z "$VERBOSE" ]]
+	then
+		./info.sh | sendmail -v "$EMAIL_TO"
+	else
+		./info.sh | sendmail -v "$EMAIL_TO" > /dev/null
+	fi
 fi
